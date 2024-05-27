00a6bab9388c2384dce54eb684843d8dff743488
arnavdaryani <adaryan@purdue.edu>
2024-05-28 01:56:38 +0530

    made changes

--- frontend/src/Components/Income/Income.js
+++ frontend/src/Components/Income/Income.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styled from 'styled-components'
 import { InnerLayout } from '../../styles/Layout';
+import { useGlobalContext } from '../../context/globalContext';
+import Form from '../Form/Form';
+import IncomeItem from '../IncomeItem/IncomeItem';
 
 function Income() {
+  const {addIncome,incomes, getIncomes} = useGlobalContext()
+
+  useEffect(() =>{
+    getIncomes()
+  }, [])
   return (
     <IncomeStyled>
       <InnerLayout>
         <h1>Incomes</h1>
         <div className="income-content">
-          <div className="form-container"></div>
+          <div className="form-container">
+            <Form />
+          </div>
           <div className="incomes">
-            
+              {incomes.map((income) => {
+                  const {_id, title, amount, date, category, description, type} = income;
+                  return <IncomeItem
+                    key={_id}
+                    id={_id} 
+                    title={title} 
+                    description={description} 
+                    amount={amount} 
+                    date={date} 
+                    category={category} 
+                    indicatorColor="var(--color-green)"
+                  />
+              })}
           </div>
         </div>
       </InnerLayout>
--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import axios from 'axios';
 
-const GloalContext = React.createContext()
+
+const BASE_URL = "http://localhost:5001/api/v1/";
+
+const GlobalContext = React.createContext()
 
 export const GlobalProvider = ({children}) => {
+
+    const [incomes, setIncomes] = useState([])
+    const [expenses, setExpenses] = useState([])
+    const [error, setError] = useState(null)
+
+    const addIncome = async(income) => {
+        const response = await axios.post(`${BASE_URL}add-income`, income)
+            .catch((err) => {
+                setError(err.response.data.message)
+            })
+    }
+
+    const getIncomes = async() => {
+        const response = await axios.get(`${BASE_URL}get-incomes`)
+        setIncomes(response.data)
+        console.log(response.data)
+    }
+
     return (
-        <GlobalContext.Provider>
+        <GlobalContext.Provider value={{
+            addIncome,
+            getIncomes,
+            incomes
+        }}>
             {children}
         </GlobalContext.Provider>
     )
+}
+
+export const useGlobalContext = () =>{
+    return useContext(GlobalContext)
 }
