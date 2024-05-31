77783d2bfc089acac92ce630cf55ae6bccffd297
arnavdaryani <adaryan@purdue.edu>
2024-06-01 05:00:03 +0530

    made changes

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -71,6 +71,19 @@ export const GlobalProvider = ({children}) => {
         return totalIncome
     }
 
+    const totalBalance = () => {
+        return totalIncome() - totalExpenses()
+    }
+
+    const transactionHistory = () => {
+        const history = [...incomes, ...expenses]
+        history.sort((a, b) => {
+            return new Date(b.createdAt) - new Date(a.createdAt)
+        })
+
+        return history
+    }
+
     return (
         <GlobalContext.Provider value={{
             addIncome,
@@ -82,7 +95,9 @@ export const GlobalProvider = ({children}) => {
             addExpense,
             getExpenses,
             deleteExpense,
-            totalExpenses
+            totalExpenses,
+            totalBalance,
+            transactionHistory
         }}>
             {children}
         </GlobalContext.Provider>
